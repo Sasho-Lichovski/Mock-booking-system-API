fb6f38dd2560b8ada6d8f183f04b6b6a6b0d5901
Sasho Lichovski <sasholichovski@gmail.com>
2024-06-17 12:47:51 +0200

    develop start

---
+++ Mock booking system API/Controllers/SearchController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mock_booking_system_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        public SearchController()
+        {
+            
+        }
+    }
+}
